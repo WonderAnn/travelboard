d42a61fc53dd09ba0bfe69e0728934310e3ffdd1
Hanna Popova <popovaas@gmail.com>
2019-06-24 18:28:53 +0300

    Add tabs and map

--- R/mod_weather.R
+++ R/mod_weather.R
@@ -12,25 +12,24 @@
 #' @rdname mod_weather
 #'
 #' @keywords internal
+#' @import leaflet
 #' @export 
 #' @importFrom shiny NS tagList 
 mod_weather_ui <- function(id, dest){
   ns <- NS(id)
-  
+  tagList(
+    tags$h1(paste(dest, "Weather", sep = "-")),
+    fluidPage(
+      tabsetPanel(id = "tabs", 
+                  tabPanel("Map", box(title = dest, leafletOutput(ns("map"), width = 800 ))),
+                  tabPanel("forecast", box(title = dest, plotOutput(ns("plot1"), height = 250)))
+             #     tabPanel("Historical", plotOutput("Plot2"))
+      )))
+     
   # This is just an example UI to be modified
   # Please change for your purpose
   # Do not forget to put ns() around all input ids!!!
-  tagList(
-    tags$h1(paste(dest, "Weather", sep = "-")),
-    fluidRow(
-      box(title = dest, plotOutput(ns("plot1"), height = 250)),
-      box(title = dest, plotOutput(ns("plot2"), height = 250)),
-      box(
-        title = "Controls",
-        sliderInput(ns("slider"), "Number of observations:", 1, 100, 50)
-      )
-    )
-  )
+  
 }
 
 # Module Server
@@ -42,6 +41,7 @@ mod_weather_ui <- function(id, dest){
 mod_weather_server <- function(input, output, session, dest){
   ns <- session$ns
   
+  
   # This is just an example Server to be modified
   # Please change for your purpose
   histdata <- rnorm(500)
@@ -51,15 +51,24 @@ mod_weather_server <- function(input, output, session, dest){
   })
   
   
-  output$plot2 <- renderPlot({
-    fname <- sprintf("~/workshop/data/weather/%s.rds", tolower(dest()))
-    dat <- readRDS(fname)
-    dat %>% 
-      mutate(date = as.Date(date)) %>% 
-      ggplot() + 
-      geom_line(aes(date, value, color = datatype, group = datatype)) +  
-      facet_wrap(~datatype, scales = "free_y", ncol = 1)
-  })
+  apikey <- "d7eae13fe954ea0e04b0c40a172c4a10"
+  owmr_settings(apikey)
+  mymap = leaflet() %>% 
+    addTiles() %>% 
+    addProviderTiles(providers$OpenWeatherMap.Temperature, options = providerTileOptions(apiKey=apikey)) %>% 
+    setView(24.80927, 35.24012, zoom = 9)
+  
+  output$map <- renderLeaflet(mymap)
+  
+#  output$plot2 <- renderPlot({
+#    fname <- sprintf("~/workshop/data/weather/%s.rds", tolower(dest()))
+#    dat <- readRDS(fname)
+#    dat %>% 
+#      mutate(date = as.Date(date)) %>% 
+#      ggplot() + 
+#      geom_line(aes(date, value, color = datatype, group = datatype)) +  
+#      facet_wrap(~datatype, scales = "free_y", ncol = 1)
+#  })
 }
 
 ## To be copied in the UI
